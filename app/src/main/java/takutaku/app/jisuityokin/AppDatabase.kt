34b09ab1c326a2e34c49055fb7136d549fa16e4c
noodle2290 <noodle2290.t@gmail.com>
2022-03-16 18:34:22 +0900

    roomのmigrationでエラー2

--- app/src/main/java/takutaku/app/jisuityokin/AppDatabase.kt
+++ app/src/main/java/takutaku/app/jisuityokin/AppDatabase.kt
@@ -1,15 +1,29 @@
 package takutaku.app.jisuityokin
 
 import androidx.room.*
+import androidx.room.migration.AutoMigrationSpec
+import androidx.sqlite.db.SupportSQLiteDatabase
+
 
 @Database(
     version = 2,
-    entities = [Memo::class],
-    autoMigrations = [
-        AutoMigration (from = 1, to = 2)
-    ],
-    exportSchema = true
-)abstract class AppDatabase : RoomDatabase() {
-    abstract fun memoDao(): MemoDao
-    @DeleteColumn(tableName = "Memo", columnName = "date")
+    entities = [Memo.class],
+autoMigrations = [
+@AutoMigration (
+    from = 1,
+    to = 2
+)
+],
+exportSchema = true
+)
+abstract class AppDatabase  : RoomDatabase() {
+    @DeleteColumn(fromTableName = "Memo", deletedColumnName = "date")
+    @DeleteColumn(fromTableName = "Memo", deletedColumnName = "check")
+    class MyExampleAutoMigration : AutoMigrationSpec {
+        @Override
+        override fun onPostMigrate(db: SupportSQLiteDatabase) {
+            // Invoked once auto migration is done
+        }
+    }
 }
+
